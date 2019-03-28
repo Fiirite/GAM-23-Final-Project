d2894b43aaed73d490992e543a8422dcf90018e8
Chives365 <chiv3s4lif3@aol.com>
2019-03-27 20:21:33 -0700

    Ship Controls

---
+++ Team 2 Space Shooter/Assets/Chives/Scripts/ShipControls.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControls : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float moveSpeed;
+    public float tiltAngle;
+
+    private void Start()
+    {
+        rb = transform.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float moveLR = Input.GetAxis("Horizontal");
+        float moveFB = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveLR, 0, moveFB);
+        rb.velocity = movement * moveSpeed;
+
+        rb.rotation = Quaternion.Euler(Vector3.forward * moveLR * -tiltAngle);
+    }
+}
