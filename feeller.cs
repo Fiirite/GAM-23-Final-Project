7890cfbf0bfc40483ff28105d526805a23428ef8
Ryan Palacios <30686955+Ryan-Palacios@users.noreply.github.com>
2019-04-15 19:03:26 -0700

    Adding Scripts

---
+++ feeller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class feeller : MonoBehaviour
+{
+    public bool canMove = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "trigger")
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.green;
+            canMove = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "trigger")
+        {
+            gameObject.GetComponent<Renderer>().material.color = Color.white;
+            canMove = false;
+        }
+    }
+}
