46652e0b2f7aebbffe168bff215d94d818c40977
Chives365 <48000847+Chives365@users.noreply.github.com>
2019-04-15 21:08:58 -0700

    working ship controls

---
+++ Team 2 Space Shooter/Assets/Barry/PlayerShipController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+    public Rigidbody rb;
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();     //The new shape to push an information from Rigidbody. I think.
+
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+         (
+          Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+          0.0f,
+          Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+          );
+
+        rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt);
+    }
+}
--- Team 2 Space Shooter/Assets/ShipControls.cs
+++ Team 2 Space Shooter/Assets/ShipControls.cs
@@ -19,9 +19,9 @@ public class ShipControls : MonoBehaviour
         float moveLR = Input.GetAxis("Horizontal");
         float moveFB = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveLR, 0, moveFB);
+        Vector3 movement = new Vector3(moveLR, moveFB, 0);
         rb.velocity = movement * moveSpeed;
 
-        rb.rotation = Quaternion.Euler(Vector3.forward * moveLR * -tiltAngle);
+        rb.rotation = Quaternion.Euler(Vector3.up * moveLR * -tiltAngle);
     }
 }
