b31aafeff5eb299c4ddb556f330e2ee1c755a53e
DevinGreen <48001077+DevinGreen@users.noreply.github.com>
2019-04-19 21:00:33 -0700

    Version2

---
+++ New folder/New Unity Project/Assets/WeaponController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+
+    public Transform player;
+    public float range = 50.0f;
+    public float bulletImpulse = 20.0f;
+
+    private bool onRange = false;
+
+    public Rigidbody projectile;
+
+    void Start()
+    {
+        float rand = Random.Range(1.0f, 2.0f);
+        InvokeRepeating("Shoot", 2, rand);
+    }
+
+    void Shoot()
+    {
+
+        if (onRange)
+        {
+
+            Rigidbody bullet = (Rigidbody)Instantiate(projectile, transform.position + transform.forward, transform.rotation);
+            bullet.AddForce(transform.forward * bulletImpulse, ForceMode.Impulse);
+
+            Destroy(bullet.gameObject, 2);
+        }
+
+
+    }
+
+    void Update()
+    {
+
+        onRange = Vector3.Distance(transform.position, player.position) < range;
+
+        if (onRange)
+            transform.LookAt(player);
+    }
+
+
+}
